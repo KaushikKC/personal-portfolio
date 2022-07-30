15ee6ea13f5d7c9424cbeeeccac316b064af79ad
KaushikKC <kccreations1704@gmail.com>
2022-07-30 12:42:48 +0530

    Updated some details

--- components/Data.tsx
+++ components/Data.tsx
@@ -1,46 +1,95 @@
-import Project1 from '../Images/p1.webp'
-import Project2 from '../Images/p2.webp'
-import Project3 from '../Images/p3.webp'
-import Project4 from '../Images/p4.webp'
+import Project1 from '../Images/Netflix.jpg'
+import Project2 from '../Images/DrumKid.jpg'
+import Project3 from '../Images/Metaverse.png'
+import Project4 from '../Images/Instagram.jpg'
+import Project5 from '../Images/Simon.jpg'
+import Project6 from '../Images/WhatsApp.jpg'
+import Project7 from '../Images/TinDog.jpg'
+import Project8 from '../Images/DisneyPlusHotstar.webp'
+import Project9 from '../Images/Tesla.jpg'
+import Project10 from '../Images/'
+import Project11 from '../Images/'
 
 
 export const projectsData = [
     {
       id: '1',
       image: Project1,
-      name: 'project name 1',
+      name: 'Netflix Clone',
       category: 'Cloned Apps',
+      URL:'https://github.com/KaushikKC/netflix-clone'
     },
     {
       id: '2',
       image: Project2,
-      name: 'project name 2',
+      name: 'Drum Kit',
       category: 'FrontEnd Works',
+      URL:'',
     },
     {
       id: '3',
       image: Project3,
-      name: 'project name 3',
+      name: 'Web3 Chating App',
       category: 'Web3 Works',
+      URL:'https://github.com/KaushikKC/Metaverse-Apps/tree/main/metaverse-project1',
     },
     {
       id: '4',
       image: Project4,
-      name: 'project name 4',
+      name: 'Instagram Clone',
       category: 'Cloned Apps',
+      URL:'https://github.com/KaushikKC/Instagram-clone',
     },
     {
       id: '5',
-      image: Project2,
-      name: 'project name 5',
+      image: Project5,
+      name: 'Simon Game',
       category: 'FrontEnd Works',
+      URL:'',
     },
     {
       id: '6',
-      image: Project4,
-      name: 'project name 6',
+      image: Project6,
+      name: 'WhatsApp Clone',
       category: 'Cloned Apps',
+      URL:'https://github.com/KaushikKC/whatsapp-clone',
     },
+    {
+      id: '7',
+      image: Project7,
+      name: 'TinDog',
+      category: 'FrontEnd Works',
+      URL:'',
+    },
+    {
+      id: '8',
+      image: Project8,
+      name: 'DisneyPlusHotstar Clone',
+      category: 'Cloned Apps',
+      URL:'https://github.com/KaushikKC/Disney-Plus-Hotstar-Clone',
+    },
+    {
+      id: '9',
+      image: Project9,
+      name: 'Tesla Clone',
+      category: 'Cloned Apps',
+      URL:'https://github.com/KaushikKC/tesla-clone',
+    },
+    {
+      id: '10',
+      image: Project10,
+      name: 'Tesla Clone',
+      category: 'Cloned Apps',
+      URL:'https://github.com/KaushikKC/tesla-clone',
+    },
+    {
+      id: '11',
+      image: Project11,
+      name: 'Tesla Clone',
+      category: 'Cloned Apps',
+      URL:'https://github.com/KaushikKC/tesla-clone',
+    },
+    
   ];
   
   // projects
